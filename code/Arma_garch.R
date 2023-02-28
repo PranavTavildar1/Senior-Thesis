be6854a3bdb64944f731163791dbc88f65daa88f
PranavTavildar1 <56324894+PranavTavildar1@users.noreply.github.com>
2023-02-28 11:57:03 -0500

    "Added Sentiment Score to arma garch model"

--- code/Arma_garch.R
+++ code/Arma_garch.R
@@ -1,10 +1,27 @@
-install.packages("rugarch")
+library(rugarch)
+library(readr)
+
 returns <- read.csv("/Users/pranavtavildar/Desktop/Senior-Thesis/data/returns.csv") # nolint
 returns_ts <- ts(returns[, 1], frequency = 252)
 arma_model <- arima(returns_ts, order = c(1, 0, 1))
-library(rugarch)
+
 garch_spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), mean.model = list(armaOrder = c(1,1)))
 garch_model <- ugarchfit(garch_spec, data = returns_ts)
 summary(arma_model)
 residuals_garch <- residuals(garch_model)
 plot(residuals_garch)
+
+sentiment <- read.csv("/Users/pranavtavildar/Desktop/Senior-Thesis/data/sentiment_scored_chevron.csv")
+
+# Merge the data frames by date
+data <- merge(returns, sentiment, by = "Date")
+
+# Create the ARMA-GARCH model
+spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+                   mean.model = list(armaOrder = c(1, 1), include.mean = TRUE),
+                   distribution.model = "norm")
+
+fit <- ugarchfit(spec, data[, 2:3])
+
+# Print the summary of the estimated model
+summary(fit)
